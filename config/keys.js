291e2e95b2f3620369058295af1c4eddfd8a9fc7
vidisharaichand <bca22138@glbim.ac.in>
2024-07-19 14:59:22 +0530

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //mongoURI: "mongodb://spectre:spectre007@ds253537.mlab.com:53537/elearning", //apurva's
+  mongoURI: 'mongodb+srv://Shoaibmalik:MALIK10022005@cluster1.s5xcto5.mongodb.net/?retryWrites=true&w=majority&appName=cluster1',
+  secretOrKey: "secret"
+};
